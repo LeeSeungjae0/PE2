0df253766f3263f579ba284cf781b6341be846ed
PC2308 <flfsks4242@hanyang.ac.kr>
2024-05-23 16:11:34 +0900

    전체 파일 분류

---
+++ src/parsing.py
@@ -0,0 +1,25 @@
+import xml.etree.ElementTree as elemTree
+import os
+import numpy as np
+
+def parse_xml_files(directory1, directory2, current_directory):
+    output_directory = os.path.join(current_directory, '..', 'res', 'HY202103', directory1, directory2)
+    os.makedirs(output_directory, exist_ok=True)
+
+    csv_file_path = os.path.join(output_directory, f'{directory1}_{directory2}.csv')
+    open(csv_file_path, 'w').close()
+
+    xml_directory = os.path.join(current_directory, '..', 'dat', 'HY202103', directory1, directory2)
+    if not os.path.isdir(xml_directory):
+        print(f"The directory {xml_directory} does not exist. Please enter a valid directory path.")
+        return
+
+    xml_files = []
+    for filename in os.listdir(xml_directory):
+        if filename.endswith('LMZC.xml') or filename.endswith('LMZO.xml'):
+            xml_file_path = os.path.join(xml_directory, filename)
+            tree = elemTree.parse(xml_file_path)
+            root = tree.getroot()
+            xml_files.append((filename, root))
+
+    return xml_files, csv_file_path, output_directory
